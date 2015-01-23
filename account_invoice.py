ea89004396b4986f9417cbb8b744c7bdcebda203
Abhimanyukit <gaurhabhimanyu@gmail.com>
2015-01-23 14:55:36 +0530

    sale and invoice work properly

--- account_invoice.py
+++ account_invoice.py
@@ -8,6 +8,14 @@ class account_invoice(models.Model):
     @api.depends('invoice_line.discount','invoice_line.quantity','invoice_line.price_unit')
     def _compute_discount(self):
         self.discount_total = sum(line.price_unit*line.quantity*float(line.discount or 0.0) / float(100.0) for line in self.invoice_line)
+        
+    @api.one
+    @api.depends('invoice_line.discount','invoice_line.quantity','invoice_line.price_unit')
+    def _compute_amounts(self):
+        self.before_discount_total = sum(line.price_unit*line.quantity for line in self.invoice_line)
     
-    discount_total = fields.Float(string='Discount', store=True, readonly=True, compute='_compute_discount', track_visibility='always',
+    discount_total = fields.Float(string='Line Discount', store=True, readonly=True, compute='_compute_discount', track_visibility='always',
                                   digits= dp.get_precision('Discount'))
+    before_discount_total = fields.Float(string='Amount', store=True, readonly=True, compute='_compute_amounts', track_visibility='always',
+                                  digits= dp.get_precision('Account'))
+    global_discount=fields.Char("Global Discount")
